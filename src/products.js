b119cc7bf4fa2b12b3eb115aeaee836b92e4add9
torresallenjhade <torresallenjhade83@gmail.com>
2024-04-23 07:59:53 +0800

    first-commit

---
+++ src/products.js
@@ -0,0 +1,73 @@
+import product1 from './assets/1.jpg';
+import product2 from './assets/2.jpg';
+import product3 from './assets/3.jpg';
+import product4 from './assets/4.jpg';
+import product5 from './assets/5.jpg';
+import product6 from './assets/6.jpg';
+import product7 from './assets/7.jpg';
+import product8 from './assets/8.jpg';
+import product9 from './assets/9.jpg';
+import product10 from './assets/10.jpg';
+
+export const PRODUCTS = [
+    {
+        id: 1,
+        productName: 'The Last Of Us Remastered',
+        price: 1095.0,
+        productImage: product1,
+    },
+    {
+        id: 2,
+        productName: 'God Of War',
+        price: 995.0,
+        productImage: product2,
+    },
+    {
+        id: 3,
+        productName: 'Marvel Spider-Man',
+        price: 2195.0,
+        productImage: product3,
+    },
+    {
+        id: 4,
+        productName: 'Marvel Spider-Man Miles Morales',
+        price: 2490.0,
+        productImage: product4,
+    },
+    {
+        id: 5,
+        productName: 'Ghost Of Tsushima',
+        price: 2990.0,
+        productImage: product5,
+    },
+    {
+        id: 6,
+        productName: 'Mortal Kombat 11 Ultimate Edition',
+        price: 1095.0,
+        productImage: product6,
+    },
+    {
+        id: 7,
+        productName: 'Bloodborne',
+        price: 1095.0,
+        productImage: product7,
+    },
+    {
+        id: 8,
+        productName: 'Tekken 7',
+        price: 895.0,
+        productImage: product8,
+    },
+    {
+        id: 9,
+        productName: 'Resident Evil 7 Biohazard',
+        price: 1050.0,
+        productImage: product9,
+    },
+    {
+        id: 10,
+        productName: 'Cyberpunk 2077',
+        price: 1395.0,
+        productImage: product10,
+    }
+];
